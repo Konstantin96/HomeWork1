de45dab0716893ec8fae567edf00d231f02efbd1
Lon3L1ness <Lon3L1ness@DESKTOP-1F6HP4V>
2017-12-06 15:49:02 +0600

    Upload 2 lvl

--- HomeWork27.11.2017/Source.cpp
+++ HomeWork27.11.2017/Source.cpp
@@ -31,11 +31,38 @@ start:
 	{
 		//   Напишите функцию int f(int x, int y), которая возвращает 0, если значения x и y оба равны нулю, 12 / x, 
 		// если y равен 0, 12 / y, если x равен 0, иначе 144 / (x*y)
-		int f(int x, int y);
 		int x, y;
-		
-		
+		cout << "Введите x : ";
+		cin >> x;
+		cout << "Введите y : ";
+		cin >> y;
+		{
+			if (x == 0 && y == 0)
+			{
+				cout << (x = 0)<<endl;
+				cout << (y = 0) << endl;
+			}
+
+			else if (x != 0 && y == 0)
+			{
+				cout << 12 / x << endl;
+			}
+			else if (x == 0 && y != 0)
+			{
+				cout << 12 / y << endl;
+			}
+			else
+			{
+				cout << 144 / (x*y) << endl;
+			}
+		}
 	}
+
+	else if ( nz == 3 )
+	{
+
+	}
+
 	else if (nz >= 0)
 	{
 		cout << "Такого задания несуществует!\n";
